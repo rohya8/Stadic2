c61496a65adbd9ed2f92d17b37d9fdb365ea9c16
Rajesh Mangale <rajeshmangale0802@gmail.com>
2015-08-18 19:46:03 +0530

    SPLASH SCREEN

---
+++ src/com/resoneuronance/stadic/SplashScreen.java
@@ -0,0 +1,57 @@
+package com.resoneuronance.stadic;
+
+import com.resoneuronance.stadic.student.MainStudentProfile;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class SplashScreen extends Activity 
+{
+
+	Typeface roboto;
+	TextView name;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash_screen);
+
+		name = (TextView)findViewById(R.id.splash_stadic_textview);
+
+		roboto = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Regular.ttf");
+		name.setTypeface(roboto);
+
+
+		Thread thread = new Thread() 
+		{
+
+
+			public void run() {
+
+				try {
+					sleep(2000);
+
+
+					startActivity(new Intent(getApplicationContext(),MainStudentProfile.class));
+
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+
+			}
+
+
+		};
+
+		thread.start();
+
+	}
+}
