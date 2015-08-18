442c76644e2246d666650f11240e2c80e992a4d0
ack14 <ajinkyashiva@gmail.com>
2015-08-19 01:50:52 +0530

    Constants

---
+++ src/com/resoneuronance/stadic/main/AndroidConstants.java
@@ -0,0 +1,9 @@
+package com.resoneuronance.stadic.main;
+
+public interface AndroidConstants {
+
+	String REG_ID = "regId";
+	String TAG = "STADIC";
+	String GOOGLE_PROJECT_ID = "282727282368";
+	String MESSAGE_KEY = "message";
+}
